e96c347e229845d934059690f39c4c681ab1fa0b
Ismael Celis <ismaelct@gmail.com>
2011-09-22 23:42:03 +0100

    Documented ApiBee.setup

--- lib/api_bee.rb
+++ lib/api_bee.rb
@@ -3,6 +3,34 @@ module ApiBee
   
   class << ApiBee
     
+    # Setup and instantiates a new API proxy (ApiBee::Proxy)
+    # by wrapping a bundled or custom API adapter and passing optional arguments
+    #
+    # When a hash is passed as the adapter class, it will look for that class file in 
+    # the bundled adapters directory:
+    #
+    #    ApiBee.setup(:hash, {})
+    #
+    # Looks for ApiBee::Adapters::Hash in lib/api_bee/adapters
+    #
+    # You can pass a custom adapter class
+    #
+    # Example:
+    #
+    #   class MyAdapter
+    #     def initialize(api_key)
+    #      @url = 'http://myservice.com'
+    #     end
+    #
+    #     def get(path, options = {})
+    #       # Fetch data from your service here
+    #     end
+    #   end
+    #
+    #   api = ApiBee.setup(MyAdapter, 'MY_API_KEY')
+    #
+    # That gives you an instance of ApiBee::Proxy wrapping an instance of your MyAdapter initialized with your key.
+    #
     def setup(adapter_klass, *args)
       
       adapter = if adapter_klass.is_a?(::Symbol)
@@ -37,6 +65,16 @@ module ApiBee
     
     class Config < OpenStruct
       
+      # Delegate method calls to your wrapped adapter
+      #
+      # Example:
+      #
+      #   api = ApiBee.setup(TwitterAdapter) do |config|
+      #     config.expose :post_message, :delete_message
+      #   end
+      #
+      # Now calls to api.post_message and api.delete_message will be delegated to an instance of TwitterAdapter
+      #
       def expose(*fields)
         self.adapter_delegators = fields
       end
